4e5fc6dc05d00cb13c6b9fd27b3fcf9c94727c35
32d <105021055@live.asia.edu.tw>
2016-09-12 15:51:00 +0800

    hw00 howmwork  submit
    homework

---
+++ hw00-1/src/hw.java
@@ -0,0 +1,14 @@
+
+public class hw {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("資訊工程系 數位內容組   王浚維");
+	 
+ 
+	}
+
+}
